f1f65d2a95c0fd29ed4a211b204b840f2d07610b
Dhurv Singh <dhruvrawat3256@gmail.com>
2026-01-28 09:12:35 +0530

    [Dhruv]pens per student

---
+++ Basic5.java
@@ -0,0 +1,10 @@
+public class Basic5{
+    public static void main(String[] args){
+        System.out.println("The total pens are 14 and there are 3 people");
+        System.out.println("The pens should be devided equally between them");
+        int Remained_pens = 14%3;
+        int Per_Student = 12/3;
+        System.out.print("The pen per student is "+Per_Student);
+        System.out.println(" and the remaining pen not distributed is "+Remained_pens);
+    }
+}
