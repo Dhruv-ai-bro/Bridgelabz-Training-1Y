b4483a26c52b030c92657c2b6f27685796599a6c
Dhurv Singh <dhruvrawat3256@gmail.com>
2026-01-28 09:10:47 +0530

    [Dhruv]miles

---
+++ Basic3.java
@@ -0,0 +1,8 @@
+public class Basic3 {
+    public static void main(String[] args){
+        System.out.println("The distance in km is 10.8km");
+        float miles = (float)(10.8*1.6);
+        System.out.print("The distance 10.8km in miles is "+miles);
+        System.out.println("miles");
+    }
+}
