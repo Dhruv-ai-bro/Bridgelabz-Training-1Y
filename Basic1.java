b0f969b11e2b5501326579b8089b4ca077fa1eeb
Dhurv Singh <dhruvrawat3256@gmail.com>
2026-01-28 09:09:16 +0530

    [Dhruv]age

---
+++ Basic1.java
@@ -0,0 +1,10 @@
+public class Basic1 {
+    public static void main(String[] args){
+        System.out.println("The birth year of harry is 2000");
+        System.out.println("The current year is 2024");
+        int Birth_Year = 2000;
+        int Current_Year = 2024;
+        int age = Current_Year - Birth_Year;
+        System.out.println("Harry's age in 2024 is:"+age);
+    }
+}
