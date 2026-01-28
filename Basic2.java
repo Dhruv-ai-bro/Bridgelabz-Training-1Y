45f7b9b9c9444214f557c44703986bdbd1f94bd0
Dhurv Singh <dhruvrawat3256@gmail.com>
2026-01-28 09:10:12 +0530

    [Dhruv]Percentage

---
+++ Basic2.java
@@ -0,0 +1,10 @@
+public class Basic2 {
+    public static void main(String[] args){
+        System.out.println("Sam's marks in Maths is 94");
+        System.out.println("In physics it's 95");
+        System.out.println("In chemistry it's 96");
+        float percentage = ((94+95+96)/300f)*100;
+        System.out.print("The percentage is "+percentage );
+        System.out.println("%");
+    }
+}
